96b44737da5869acd3e1d39b199ad83d911328a5
Vladyslavua <vladgershman@gmail.com>
2023-03-06 13:54:23 +0100

    init:Get one and delete one

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,44 +13,56 @@ export const getAll = async (req, res) => {
     }
 }
 
-// export const getOne = async (req, res) => {
-//     try {
-//         const postId = req.params.id;
-//
-//         PostModel.findOneAndUpdate(
-//             {
-//             _id:postId,
-//             },
-//             {
-//                 $inc:{viewsCount: 1},
-//             },
-//             {
-//                 returnDocument:'after',
-//             },
-//             (err,doc) => {
-//                 if(err){
-//                     console.log(err);
-//                    return res.status(500).json({
-//                         message: 'Can not give articles'
-//                     });
-//                 }
-//
-//                 if(!doc){
-//                     return res.status(404).json({
-//                         message:'article not found',
-//                     });
-//                 }
-//                 res.json(doc)
-//             },
-//             );
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json({
-//             message: 'Can not get articles'
-//         })
-//     }
-// }
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
 
+
+        const post = await PostModel.findOne({_id:postId}).populate('user').exec()
+
+        res.json(post)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Can not get articles'
+        })
+    }
+};
+
+
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+      PostModel.findOneAndDelete({
+            _id:postId,
+        },{},(err,doc)=>{
+            if(err){
+                console.log(err);
+                return res.status(500).json({
+                    message:'Can not delete article'
+                });
+            }
+
+            if(!doc){
+                return res.status(404).json({
+                    message:'Article not found'
+                })
+            }
+
+            res.json({
+                success:true
+            })
+        })
+
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Can not get articles'
+        })
+    }
+};
 
 export const create = async (req, res) => {
     try {
--- index.js
+++ index.js
@@ -24,21 +24,12 @@ app.use(express.json());
 
 
 app.post('/auth/login',loginValidation,UserController.login);
-
-
-
 app.post('/auth/register',registerValidation,UserController.register);
-
-
 app.get('/auth/me',checkAuth ,UserController.getMe);
-
-
 app.get('/posts',PostController.getAll)
-// app.get('/posts/:id',PostController.getOne)
-
+app.get('/posts/:id',PostController.getOne)
 app.post('/posts',checkAuth ,postCreateValidation,PostController.create)
-
-//app.delete('/posts',postCreateValidation,PostController.remove)
+app.delete('/posts/:id',checkAuth,PostController.remove)
 
 //app.patch('/posts',postCreateValidation,PostController.update)
 
